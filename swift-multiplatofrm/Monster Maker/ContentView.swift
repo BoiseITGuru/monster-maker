c748897354392851adcc94f0dd04e9b9faea8cef
BoiseITGuru <dev@boiseitguru.com>
2023-06-25 11:16:06 -0600

    removed iOS app and create swift multiplatform app

---
+++ swift-multiplatofrm/Monster Maker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monster Maker
+//
+//  Created by BoiseITGuru on 6/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-multiplatofrm/Monster Maker/Monster_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monster_MakerApp.swift
+//  Monster Maker
+//
+//  Created by BoiseITGuru on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Monster_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
